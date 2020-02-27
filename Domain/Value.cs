7931a5432e2bc234426a42f2c60098818b30852f
Anganam <anganamnkili@gmail.com>
2020-02-27 18:49:23 +0200

    Initial commit

---
+++ Domain/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+
+        //auto implemented properties
+        public int Id {get; set;}  // ID will automatically be used as Primary key by our database
+        public string Name{get ; set;}
+
+    }
+}
---
+++ Persistence/DataContex.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext //derive from DbContext(base class)
+    {
+
+        public DataContext(DbContextOptions options) :  base(options)
+        {
+
+        }
+
+        public DbSet <Value> Values{ get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //supplies data
+            builder.Entity<Value>()
+            .HasData(
+                new Value {Id = 1, Name = "Value 101"},
+                new Value {Id = 2, Name = "Value 102"},
+                new Value {Id = 3, Name = "Value 103"}
+                
+            );
+        }
+    }
+}
